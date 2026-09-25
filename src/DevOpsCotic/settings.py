eb63a3c4e76d54409df05c9d98a2705fc936609e
Leandro Castro <leandroapcastro@gmail.com>
2026-09-25 09:41:58 -0300

    Corrigir Editar Setting3

--- src/DevOpsCotic/settings.py
+++ src/DevOpsCotic/settings.py
@@ -75,7 +75,8 @@ WSGI_APPLICATION = 'DevOpsCotic.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
+
     }
 }
 
